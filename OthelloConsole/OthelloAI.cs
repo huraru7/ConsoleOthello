16142f464c56a67a7f2843f79fec3e57bac105e6
huraru <t.kouske.528@gmail.com>
2025-12-12 21:22:02 +0900

    [Updata]デバッグ用のLog処理を追加

--- OthelloConsole/OthelloAI.cs
+++ OthelloConsole/OthelloAI.cs
@@ -22,6 +22,7 @@ public class OthelloAI
             case AIStrength.normal: _depth = 4; break;
             case AIStrength.professional: _depth = 6; break;
         }
+        DebugLog($"探索深さ: {_depth}");
 
         DebugLog("=============Ai評価開始=============");
         int maxScore = int.MinValue;
@@ -29,10 +30,13 @@ public class OthelloAI
         {
             MainGameData newGameData = _gamedata.Clone();
             _recursionsCount++;
+            DebugLog($"試行純手:({move.x},{move.y}) 深さ:{_depth}スタート");
             PlacePiece(move.x, move.y, newGameData);
+            DebugLog($"盤面状態:\n{BoardToString(newGameData._tiles)}");
             int score = -Negamax(_depth - 1, newGameData, false);
             if (score > maxScore)
             {
+                DebugLog($"スコア更新:({move.x},{move.y}) スコア:{score}");
                 maxScore = score;
                 bestMove = move;
             }
@@ -48,6 +52,7 @@ public class OthelloAI
     {
         if (_depth <= 0)//深さ0は評価値を返す
         {
+            DebugLog("深さ0到達");
             return evaluationFunction(_gamedata);
         }
 
@@ -56,10 +61,15 @@ public class OthelloAI
         if (validMoves.Count == 0)
         {
             //2回連続で置けない場合はゲームは終了している。
-            if (trigger) return evaluationFunction(_gamedata);
+            if (trigger)
+            {
+                DebugLog("2回連続スキップでゲーム終了");
+                return evaluationFunction(_gamedata);
+            }
 
             //スキップする処理
             _recursionsCount++;
+            DebugLog("スキップ発生");
             int score = -Negamax(_depth - 1, _gamedata, true);
             maxScore = Math.Max(maxScore, score);
         }
@@ -75,21 +85,21 @@ public class OthelloAI
         return maxScore;
     }
 
-    // private string BoardToString(int[,] board)
-    // {
-    //     int size = board.GetLength(0);
-    //     var sb = new System.Text.StringBuilder();
-    //     for (int y = 0; y < size; y++)
-    //     {
-    //         for (int x = 0; x < size; x++)
-    //         {
-    //             sb.Append(board[y, x] switch { 0 => "-", 1 => "●", 2 => "○", _ => "?" });
-    //             sb.Append(" ");
-    //         }
-    //         sb.AppendLine();
-    //     }
-    //     return sb.ToString();
-    // }
+    private string BoardToString(int[,] board)
+    {
+        int size = board.GetLength(0);
+        var sb = new System.Text.StringBuilder();
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                sb.Append(board[y, x] switch { 0 => "-", 1 => "●", 2 => "○", _ => "?" });
+                sb.Append(" ");
+            }
+            sb.AppendLine();
+        }
+        return sb.ToString();
+    }
 }
 
 public enum AIStrength
--- OthelloConsole/OthelloDebugLog.cs
+++ OthelloConsole/OthelloDebugLog.cs
@@ -7,7 +7,7 @@ public static class OthelloDebugLog
         if (!_isDebug) return;
 
         string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
-        string logFile = $"AI_log_{timestamp}.txt";
+        string logFile = $"Log/AIlog_{timestamp}.txt";
         sw = new StreamWriter(logFile, false); // 新規作成
         Console.WriteLine($"ログファイル生成先: {Path.GetFullPath(logFile)}");
     }
